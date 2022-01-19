d84759b2530de2b968c1bd66a9eb19736e055ad5
Rebecca Sundquist <rsundquist629@gmail.com>
2022-01-19 11:06:02 -0500

    added config controllers

---
+++ config/config.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// create a connection to database
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3001
+})
+
+module.exports = sequelize;
